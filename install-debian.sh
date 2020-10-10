916e3eadf7d7f3e4322b4e1a00860b4fc80c4f7a
Asaf Niv <asafniv511@icloud.com>
2020-10-10 16:22:32 +0300

    revert

--- install-debian.sh
+++ install-debian.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-sudo make modules_install
+sudo make INSTALL_MOD_STRIP=1 modules_install
 sudo make headers_install INSTALL_HDR_PATH=/usr/include
 sudo make install
-cd /lib/modules/5.8.14+/
-find . -name *.ko -exec strip --strip-unneeded {} +
