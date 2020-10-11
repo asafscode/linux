7fef12a1957403a360adbf2488378c98c26bce4c
Asaf Niv <asafniv511@icloud.com>
2020-10-11 09:13:45 +0300

    kill this ptrace anti debug stuff

--- kernel/ptrace.c
+++ kernel/ptrace.c
@@ -472,25 +472,22 @@ out:
  */
 static int ptrace_traceme(void)
 {
-	int ret = -EPERM;
-
 	write_lock_irq(&tasklist_lock);
 	/* Are we already being traced? */
 	if (!current->ptrace) {
-		ret = security_ptrace_traceme(current->parent);
 		/*
 		 * Check PF_EXITING to ensure ->real_parent has not passed
 		 * exit_ptrace(). Otherwise we don't report the error but
 		 * pretend ->real_parent untraces us right after return.
 		 */
-		if (!ret && !(current->real_parent->flags & PF_EXITING)) {
+		if (!(current->real_parent->flags & PF_EXITING)) {
 			current->ptrace = PT_PTRACED;
 			ptrace_link(current, current->real_parent);
 		}
 	}
 	write_unlock_irq(&tasklist_lock);
-
-	return ret;
+	// Some CTFs use this trick to detect debuggers so let's just lie to them
+	return 0;
 }
 
 /*
